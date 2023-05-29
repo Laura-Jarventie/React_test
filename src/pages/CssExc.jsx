87eacf635132875b44bfcd73db5f94d27e4c8dc7
laura jarventie <laurajarventie@hotmail.com>
2023-05-29 19:47:51 +0300

    css excercise

--- src/pages/CssExc.jsx
+++ src/pages/CssExc.jsx
@@ -3,12 +3,17 @@ import "./CssExc.css";
 
 export const CssExc = () => {
     return (
-        
-        <div className="body">
-            <div>kjhkjhkjh</div>
-   <div className="circle"></div>
-   <div className="rectangle"></div>
-</div>
-
+        <div className="container"> 
+        <div className="circle"></div>
+        <div className="rectangle"></div>
+        <div className="rectangle_smaller">Suprise!</div>
+        <div className="underline">
+        <h1>Give me underline!</h1>
+        </div>
+        <div className="moving_circle"></div>
+        <div className="borders">
+          <h3>Give me borders!</h3>
+        </div>
+      </div>
     )
 }
